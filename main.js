1dc193e48906e5c528e450ecd04341c2df6dae86
z2024z <zhouzhengfa2024@outlook.com>
2024-12-14 20:26:52 +0800

    als

--- main.js
+++
@@ -1,7 +0,0 @@
-// 2024-12-14 00:19:36 union lotto [1], Lottery [2] , schedule [3] , 
-function main(){
-   const sqlite3_function = require('./function_js/sqlite3_function.js');
-   sqlite3_function.sqlite3_db();
-}
-
-main();
