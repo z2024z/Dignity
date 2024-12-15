941c3ec35dd9dde87e8d9899898050017b3f4d2b
z2024z <zhouzhengfa2024@outlook.com>
2024-12-15 16:19:16 +0800

    change

---
+++ function/Schedule.js
@@ -0,0 +1,3 @@
+// basic consumption :breakfast-9 ,lunch-15 ,dinner-14
+// update into database as sqlite3
+ 
---
+++ main.js
@@ -0,0 +1,2 @@
+// [Lottery 2024-12-14 20:31:02]
+// I want all the information I need to face today.
