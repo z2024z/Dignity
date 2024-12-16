f31b20e03a6f70bb03bcd4dace423fe16dbdca8c
z2024z <zhouzhengfa2024@outlook.com>
2024-12-16 22:04:41 +0800

    un

--- function/Schedule.js
+++ function/Schedule.js
@@ -2,4 +2,5 @@
 // update into database as sqlite3
 
 // unity game sss. need quick iteration. activate dopamine. refer to some roguelike -game as vampire_survivors, through_god_list[通神榜]
-// 
+// automated generation of maps. and monsters.
+
--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 // [Lottery 2024-12-14 20:31:02]
 // I want all the information I need to face today.
+main()
+
+function main() {
+  console.log("1.what am I going to face today?");
+
+}
+
+
+
